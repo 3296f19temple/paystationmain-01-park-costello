037ec0ca44b6ca34d12d418db3f84b0e1793aea5
nmcostello <noah.costello@temple.edu>
2019-10-01 20:05:38 -0400

    Wrote code for ProgressiveRateStrategy to enable it to pass all of the tests

--- src/paystation/domain/RateStrategy.java
+++ src/paystation/domain/RateStrategy.java
@@ -22,16 +22,16 @@ class ProgressiveRateStrategy implements RateStrategy {
     @Override
     public int calculateTime(int moneyInserted) {
         int timeBought  = 0;
-//
-//        if(moneyInserted < 150){
-//            timeBought = moneyInserted * 2 / 5;
-//        }
-//        else if(moneyInserted >= 150 && moneyInserted < 350){
-//            timeBought = ((moneyInserted - 150)* 3 / 10) + 60;
-//        }
-//        else if(moneyInserted >= 350){
-//            timeBought = ((moneyInserted - 350) / 5) + 120;
-//        }
+
+        if(moneyInserted < 150){
+            timeBought = moneyInserted * 2 / 5;
+        }
+        else if(moneyInserted >= 150 && moneyInserted < 350){
+            timeBought = ((moneyInserted - 150)* 3 / 10) + 60;
+        }
+        else if(moneyInserted >= 350){
+            timeBought = ((moneyInserted - 350) / 5) + 120;
+        }
         return timeBought;
     }
 
